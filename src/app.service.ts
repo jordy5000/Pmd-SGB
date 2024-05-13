6334ee2499ee1efc9990bd14cc5ef8fead1f891d
Jordan NGUEKO <162326093+jordy5000@users.noreply.github.com>
2024-05-14 00:47:44 +0200

    Update app.service.ts

--- src/app.service.ts
+++ src/app.service.ts
@@ -2,10 +2,16 @@ import { Injectable } from '@nestjs/common';
 
 @Injectable()
 export class AppService {
-  getHello(): string {
-    return 'Hello PM!';
+  //getHello(): string {
+  // return 'Hello PM!';
+  //}
+  //postHello(): string {
+  //  return 'PMD';
+  //}
+  getClient(body): string {
+    return `${body.name}`;
   }
-  postHello(): string {
-    return 'PMD';
+  postClient(body): string {
+    return `${body.name}`;
   }
 }
