c73334531a14e80240c9bcc0fc6ce21a41a31edb
Jordan NGUEKO <162326093+jordy5000@users.noreply.github.com>
2024-05-14 00:46:15 +0200

    Add files via upload

---
+++ app.controller.ts
@@ -0,0 +1,28 @@
+import { Controller, Get, Post} from '@nestjs/common';
+import { AppService } from './app.service';
+import { get } from 'http';
+
+@Controller()
+export class AppController {
+  constructor(private readonly appService: AppService) {}
+
+  //@Get()
+  //getHello(): string {
+  //  return this.appService.getHello();
+  //}
+
+  //@Post()
+  //postHello(): string {
+  //  return this.appService.postHello();
+  //}
+
+  @Get()
+  getClient(@Body()body: []): string {
+    return this.appService.getClient(body);
+  };
+
+  @Post()
+  postClient(@Body()body: []): string {
+    return this.appService.postClient(body);
+  }
+}
